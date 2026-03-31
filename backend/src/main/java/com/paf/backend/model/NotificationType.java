51280db193ba8fc25ed25404c23fcc86f62d7685
IBL11 <indumini.bh@gmail.com>
2026-03-31 19:45:00 +0530

    Define notification type enum

---
+++ backend/src/main/java/com/paf/backend/model/NotificationType.java
@@ -0,0 +1,10 @@
+package com.paf.backend.model;
+
+public enum NotificationType {
+    BOOKING_APPROVED,
+    BOOKING_REJECTED,
+    BOOKING_CANCELLED,
+    TICKET_STATUS_CHANGED,
+    NEW_COMMENT,
+    TECHNICIAN_ASSIGNED
+}
